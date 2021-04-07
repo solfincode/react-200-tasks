c7cbb3627f97d2af3cc0efcfd26a4b0626355a1d
Q <coderinch@gmail.com>
2021-04-07 19:39:16 +0900

    ch3 make component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import ImportedComponent from "./components/ImportedComponent";
 
 function App() {
   return (
     <div>
       <h1>Start React 200</h1>
       <p>html in jsx</p>
+      <ImportedComponent />
     </div>
   );
 }
---
+++ src/components/ImportedComponent.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class ImportedComponent extends Component {
+  render() {
+    return <h2>This is imported component</h2>;
+  }
+}
