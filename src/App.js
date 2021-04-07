ed146aa452c05a114d386bbb843164bd2adf1390
Q <coderinch@gmail.com>
2021-04-07 19:52:11 +0900

    ch5 constructor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import ImportedComponent from "./components/ImportedComponent";
 import RenderFunc from "./components/RenderFunc";
+import Constructor from "./components/Constructor";
 
 function App() {
   return (
     <div>
       <h1>Start React 200</h1>
       <p>html in jsx</p>
-      <ImportedComponent />
-      <RenderFunc />
+      <Constructor />
     </div>
   );
 }
