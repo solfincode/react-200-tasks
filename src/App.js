d6b0b2e0289db943678a3dca92f79e56cddbee4e
Q <coderinch@gmail.com>
2021-04-12 21:25:49 +0900

    ch7 componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import ImportedComponent from "./components/ImportedComponent";
 import RenderFunc from "./components/RenderFunc";
 import Constructor from "./components/Constructor";
 import GetDerivedStateFromProps from "./components/GetDerivedStateFromProps";
-
+import ComponentDidMount from "./components/ComponentDidMount";
 function App() {
   return (
     <div>
       <h1>Start React 200</h1>
       <p>html in jsx</p>
-      <GetDerivedStateFromProps prop_value="getDerievedStateFromProps" />
+      <ComponentDidMount prop_value="componentDidMount" />
     </div>
   );
 }
