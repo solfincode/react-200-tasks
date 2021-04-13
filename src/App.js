99c5473754687d6694728cea425c09423c8ead24
Q <coderinch@gmail.com>
2021-04-13 18:55:33 +0900

    ch12 class prototype

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import "./App.css";
-import ImportedComponent from "./components/ImportedComponent";
-import RenderFunc from "./components/RenderFunc";
-import Constructor from "./components/Constructor";
-import GetDerivedStateFromProps from "./components/GetDerivedStateFromProps";
-import ComponentDidMount from "./components/ComponentDidMount";
-import ShouldComponentUpdate from "./components/ShouldComponentUpdate";
-import ES6 from "./components/ES6.js";
-import SpreadOperator from "./components/SpreadOperator.js";
+import ClassPrototype from "./components/ClassPrototype.js";
+
 function App() {
   return (
     <div>
       <h1>Start React 200</h1>
       <p>html in jsx</p>
-      <SpreadOperator />
+      <ClassPrototype />
     </div>
   );
 }
---
+++ src/components/ClassPrototype.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+export default class ClassPrototype extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    //es5 prototype
+    let countFunc = (function () {
+      function count(nums) {
+        this.numbers = nums;
+      }
+      count.prototype.showNum = function () {
+        console.log(this.numbers);
+      };
+      return count;
+    })();
+    let countProto = new countFunc("from prototype " + "100");
+    countProto.showNum();
+    //es6 class
+    class CountClass {
+      constructor(num) {
+        this.number = num;
+      }
+      showNum() {
+        console.log(`${this.number}`);
+      }
+    }
+
+    //init instance
+    let count = new CountClass("from class " + "hundred");
+    count.showNum();
+  }
+  render() {
+    return (
+      <div>
+        <h2>class and prototype component</h2>
+      </div>
+    );
+  }
+}
